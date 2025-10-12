89f7668b2fd58d64f50026d32ec8bc570981c49d
rin <sayuri.shindo@gmail.com>
2025-10-12 15:11:39 +0900

    コメント追加

--- src/utilities/types.tsx
+++ src/utilities/types.tsx
@@ -2,12 +2,14 @@
 // ＊外部から呼び出すのが全体の定義⇒export を付与
 
 // 個々のポケモンのデータ型
+// オブジェクト構造のため interfaceで定義
 export interface PokemonResult {
   name: string;
   url: string;
 }
 
 // ポケモンAPIから取得したデータをjsonに直した型
+// オブジェクト構造のため interfaceで定義
 export interface PokemonListResponse {
   count: number; // ポケモンデータの総数
   next: string | null; // 次のページへのURL (文字列、または最終ページの場合は null)
