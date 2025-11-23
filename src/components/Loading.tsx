08f0772761c0d60e4055a8884835b1e872c90295
rin <sayuri.shindo@gmail.com>
2025-11-23 22:16:36 +0900

    未使用のデフォルトインポートを削除

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../scss/Loading.scss';
 import type { BallDetails } from '../utilities/typesUtility';
 import { balls } from '../utilities/dataInfo';
