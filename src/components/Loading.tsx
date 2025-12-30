ddc463d9a228f02df6d050ab3dbbd1c78ad26d9f
rin <sayuri.shindo@gmail.com>
2025-12-30 18:42:00 +0900

    ランダムロジックの修正＋abort追加

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -7,17 +7,23 @@ import { balls } from '../utilities/dataInfo';
 
 function Loading() {
   // ボール種類をセット
+  // ※index[0]=初期化用ダミーデータ
   const [ballType, setBallType] = useState<BallDetails>(balls[0]);
 
   // どのボールを表示するかランダムで選ぶ
   const randomBallSelect = (): void => {
-    const randomBallNum: number = Math.floor(Math.random() * balls.length);
+    const randomBallNum: number = Math.floor(Math.random() * balls.length) + 1;
     setBallType(balls[randomBallNum]);
   };
 
   // コンポーネントを呼び出す初回だけrandom処理実行
   useEffect(() => {
+    const controller = new AbortController();
     randomBallSelect();
+    return () => {
+      // 1回目の実行（マウント）直後に呼ばれるため、リクエストをキャンセルする
+      controller.abort();
+    };
   }, []);
 
   return (
