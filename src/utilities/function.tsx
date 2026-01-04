b23ab09ce9577726e28aa471a30360146ceb11c0
rin <sayuri.shindo@gmail.com>
2026-01-04 22:37:24 +0900

    ローディング処理の土台

--- src/utilities/function.tsx
+++ src/utilities/function.tsx
@@ -1,21 +1,23 @@
 /* 各種機能記述ファイル */
 
 /* 設定・導入 */
-import type { Result, ResultAsync } from 'neverthrow'; // 非同期処理用ライブラリ
-import type { FetchError, PokemonDetailAndURL } from './typesFetch'; // PokemonListResponse型を使用（type{型}）
-import type { setBoolean, setURL, setTypePokemonDetailData } from './types-utility';
-import { getNowCount, fetchPokemonData } from './fetchPokemon'; // getAllPokemon関数を呼び出し
+import { ok, type Result, type ResultAsync } from 'neverthrow'; // 非同期処理用ライブラリ
+import type { FetchError, PokemonListResponse, PokemonDetail, PokemonSpeciesDetail } from './typesFetch'; // PokemonListResponse型を使用（type{型}）
+import type { setBoolean, setPokemonData, LsPokemon, PokedexNumber } from './typesUtility';
+import { getNowAPI, getPokemonDetail } from './fetchPokemon'; // fetchPokemonから各関数を呼び出し
+import { storageAvailable, getEndID, getNationalData, getJaData, getPokedexNumber, getDisplayImg, createNullSpecies, mergeAndUniqueById } from './utilityFunction';
+import { parseJsonBody, alertError } from './fetchFunction';
 
 /***  処理記述 ***/
 
-// 画面初回ロード時に行う処理
+// 画面初回ロード時に行うメイン処理
 /*** @name loadProcess
- *   @function
+ *   @function arrow, async/await
  *   @param initialURL:string(ポケモンAPI)
- *   @param setPreURL:setURL(前ページURL更新,useState)
- *   @param setNextURL:setURL(次ページURL更新,useState)
+ *   @param pokemonData:LsPokemon[](表示・検索用ポケモンデータ
  *   @param setIsLoading:setBoolean(ローディング判定,useState)
- *   @param setPokemonDetailData:setTypePokemonDetailData(個々のポケモンデータの配列)
+ *   @param setPokemonData:setPokemonData(API取得データ,useState)
+ *   @param signal:AbortSignal fetch操作を止めるシグナル
  *   @return void
  * 
   1. fetchで更新があるか確認
@@ -29,96 +31,385 @@ import { getNowCount, fetchPokemonData } from './fetchPokemon'; // getAllPokemon
   3-3. 表示用データを画面に表示
   3-4. バックグラウンドで残りのデータを50件ずつ取得・格納
 */
-export const loadProcess = (initialURL: string, setIsLoading: setBoolean): void => {
-  // 1. fetchで更新があるか確認
-  firstDataCheck(initialURL);
+export const loadProcess = async (initialURL: string, pokemonData: LsPokemon[], setIsLoading: setBoolean, setPokemonData: setPokemonData, signal: AbortSignal) => {
+  // 一度に取得するAPIの数
+  const getAPIcount: number = 1;
 
-  // ローディング解除
+  /* どのルートでも最新のフロントAPIを一度叩く */
+  //
+  // firstDataCheckでAPIの最新状況を取得する
+  const nowFetchResult: Result<PokemonListResponse, FetchError> = await firstDataCheck(initialURL, signal);
+
+  // 一連のfetch中のエラーここで最終処理
+  if (nowFetchResult.isErr()) {
+    // 画面にエラー内容表示
+    alertError(nowFetchResult);
+    return; // 関数実行終了
+  }
+
+  // APIの結果から最新の全国図鑑の番号を切り出し・取得
+  const pokedexNumArray: number[] = getEndID(nowFetchResult.value.results);
+
+  // バックグラウンド処理に移行するかのフラグ
+  let isContinue = true;
+  const currentLsCount = Number(localStorage.getItem('pokeRegNum'));
+
+  console.log({ pokedexNumArray });
+  console.log('pokedexNumArray.length' + pokedexNumArray.length);
+
+  console.log('確認');
+  console.log(nowFetchResult.value.count);
+  console.log('LS: ' + storageAvailable('localStorage'));
+  console.log(localStorage.getItem('pokeRegNum'));
+  console.log(currentLsCount === nowFetchResult.value.count);
+
+  if (storageAvailable('localStorage') && localStorage.getItem('pokeRegNum') && currentLsCount === nowFetchResult.value.count) {
+    // ・ローカルストレージが使える
+    // ・ローカルストレージに既存データがある
+    // ・ローカルストレージのデータ数とAPIのデータ数が同じ
+    // ⇒LSに登録済みのデータを使う
+    isContinue = false; // fetchの追加処理は不要
+  } else {
+    // 上記３点を１つでも満たさない
+    // ⇒APIからデータを取ってくる
+    getNowPokemonData(pokedexNumArray, currentLsCount, 20, pokemonData, setPokemonData, signal);
+  }
+
+  // ローディング画面解除
   setIsLoading(false);
+
+  // fetch処理継続
+  if (isContinue) {
+    // バックグラウンドで行う＝同期処理＝awaitつけない
+    backgroundFetchAPI(pokedexNumArray, getAPIcount, pokemonData, setPokemonData, signal);
+  }
 };
 
+//
+//
 // fetchで更新があるか確認
 /*** @name loadProcess
- *   @function
+ *   @function arrow, async/await
  *   @param initialURL:string(ポケモンAPI)
- *   @param setPreURL:setURL(前ページURL更新,useState)
- *   @param setNextURL:setURL(次ページURL更新,useState)
- *   @param setIsLoading:setBoolean(ローディング判定,useState)
- *   @param setPokemonDetailData:setTypePokemonDetailData(個々のポケモンデータの配列)
- *   @return void
- * 
-  1. fetchで更新があるか確認
-*/
-const firstDataCheck = (initialURL: string) => {
+ *   @param signal:AbortSignal fetch操作を止めるシグナル
+ *   @return Promise<number>
+ *
+ */
+const firstDataCheck = async (initialURL: string, signal: AbortSignal): Promise<ResultAsync<PokemonListResponse, FetchError>> => {
   // ポケモンAPIからカウントを取得
-  const nowCount: ResultAsync<number, FetchError> = getNowCount(initialURL);
-  console.log(nowCount);
+  const nowApiResult: Result<PokemonListResponse, FetchError> = await getNowAPI(initialURL, signal);
+
+  // 一連のfetch中にエラー発生⇒先に戻す
+  if (nowApiResult.isErr()) {
+    const fetchError: FetchError = nowApiResult.error;
+    console.error(`[データ取得失敗] エラータイプ: ${fetchError.type}`, fetchError);
+
+    return nowApiResult; // Errが返る
+  }
+
+  // 正常終了時⇒呼び出し元の処理を進める
+  return ok(nowApiResult.value);
 };
 
-// ポケモンAPIから情報を取得する非同期処理
-/*** @name fetchPokemonData
- *   @function
- *   @param url:string(ポケモンAPI)
- *   @param setPreURL:setURL(前ページURL更新,useState)
- *   @param setNextURL:setURL(次ページURL更新,useState)
- *   @param setIsLoading:setBoolean(ローディング判定,useState)
- *   @param setPokemonDetailData:setTypePokemonDetailData(個々のポケモンデータの配列)
- *   @return Promise<void> :async仕様⇒Promise型
- *  ポケモンAPIからデータを取得・加工する全体処理
- *  neverthrow構文使用
- *  内部関数のエラーの結果は、すべてFetchErrorに格納されて排出される
+//
+//
+// 最新のポケモン情報を取得する一式
+/*** @name getNowPokemonData
+ *   @function arrow, async/await
+ *   @param pokedexNumArray:number[](ポケモン管理番号)
+ *   @param start:number(開始配列要素番号)
+ *   @param run:number(実行件数)
+ *   @param pokemonData:LsPokemon[](表示・検索用ポケモンデータ
+ *   @param setPokemonData:setPokemonData(API取得データ,useState)
+ *   @param signal:AbortSignal fetch操作を止めるシグナル
+ *   @return Promise<number>
+ *   ・ローカルストレージからデータを取得できない
+ *   ・ローカルストレージ保存の内容からAPI側が更新されている
  */
-export const asynchroFunction = async (url: string, setPreURL: setURL, setNextURL: setURL, setIsLoading: setBoolean, setPokemonDetailData: setTypePokemonDetailData): Promise<void> => {
-  // 非同期処理でAPIから情報取得処理を定義
+const getNowPokemonData = async (pokedexNumArray: number[], start: number, run: number, pokemonData: LsPokemon[], setPokemonData: setPokemonData, signal: AbortSignal) => {
+  // 処理する範囲を指定
+  const runNumbers: number[] = pokedexNumArray.slice(start, start + run);
 
-  // fetch処理一式実行
-  // エラー含めResult型で結果が戻ってくる
-  const resultPokemonData: Result<PokemonDetailAndURL, FetchError> = await fetchPokemonData(url);
+  // ⇒ポケモンAPIから最新データを取得（基本情報）
+  const pokemonDetails: Result<PokemonDetail[], FetchError> = await getPokemonData<PokemonDetail>(runNumbers, 'pokemon', signal);
+  // 一連のfetch中のエラーここで最終処理
+  if (pokemonDetails.isErr()) {
+    // 画面にエラー内容表示
+    alertError(pokemonDetails);
+    return; // 関数実行終了
+  }
 
-  // resultPokemonData:Resultは成功も失敗も内包⇒matchで分岐処理
-  resultPokemonData.match(
-    // 成功：変数pokemonData
-    (resPokemonData) => {
-      // 前ページ情報取得URLを格納
-      setPreURL(resPokemonData.previous);
+  // 取得した結果から種類（species）番号を取得
+  const tmpSpeciesNum: number[] = getSpeciesNumber(pokemonDetails.value);
 
-      // 次ページ情報取得URLを格納
-      setNextURL(resPokemonData.next);
+  // ⇒ポケモンAPIから最新データを取得（種類別情報）
+  const pokemonSpeciesResult: Result<PokemonSpeciesDetail[], FetchError> = await getPokemonData<PokemonSpeciesDetail>(tmpSpeciesNum, 'pokemon-species', signal);
 
-      // 結果をpokemonDetailDataに格納（更新）
-      setPokemonDetailData(resPokemonData.pokemonDetailData);
-    },
-    // 失敗：変数fetchError
-    (fetchError) => {
-      // FetchError を処理
-      console.error(`[データ取得失敗] エラータイプ: ${fetchError.type}`, fetchError);
+  // species未登録データもある
+  // ⇒404で返ってくることもあるのでエラーでも後続処理続行
+  const pokemonSpecies: PokemonSpeciesDetail[] = pokemonSpeciesResult.match(
+    (successData) => successData, // 成功時はそのままデータ保持
+    (resultError) => {
+      // 該当番号のPokemonSpeciesDetail型をnullで埋めたデータを返す
+      console.warn(`id:${tmpSpeciesNum}, species未登録。${resultError.status}`);
+      return tmpSpeciesNum.map((id): PokemonSpeciesDetail => createNullSpecies(id));
     },
   );
 
-  // 画面をトップにスクロールして戻す
-  window.scroll({ top: 0 });
+  // ※matchで成否の処理後なので、全て成功後のPokemonSpeciesDetail型として扱う
+  // 2つのAPIから取得した情報でオブジェクトの配列をつくる
+  // createBaseDataが確実に終わってからローカルストレージの更新
+  const regLsData: LsPokemon[] = createBaseData(pokemonDetails.value, pokemonSpecies, runNumbers);
 
-  // ローディング解除
-  setIsLoading(false);
+  if (storageAvailable('localStorage')) {
+    // ローカルストレージのデータを更新する
+    console.log('updateLsData');
+    updateLsData(regLsData);
+  } else {
+    // ローカルストレージが使用できない場合はメモリで管理
+    console.log('addDataToMemory');
+    addDataToMemory(regLsData, pokemonData, setPokemonData);
+  }
 };
 
-// ページ遷移処理
-/*** @name movePage
- *   @function:void（戻り値無）
- *   @param url:string(ポケモンAPI)
- *   @param setPreURL:setURL(前ページURL更新,useState)
- *   @param setNextURL:setURL(次ページURL更新,useState)
- *   @param setIsLoading:setBoolean(ローディング判定,useState)
- *   @param setPokemonDetailData:setTypePokemonDetailData(個々のポケモンデータの配列)
- *   @return void
- *  前後のページに遷移・該当ページに表示するポケモン情報を取得／表示する
- *  処理が同じなのでブラウザ表示時のasynchroFunction()を使用する
+//
+//
+// ポケモン個別APIで詳細データを取得する
+/*** @name getPokemonData
+ *   @function async/await
+ *   @param runPokedexNumbers[]:number[](全国図鑑の番号)
+ *   @param endPoint:string(実行先APIのURLのパーツ)
+ *   @param signal:AbortSignal fetch操作を止めるシグナル
+ *   @return Promise<ResultAsync<PokemonDetail[], FetchError>>
+ *  ・詳細データの取得
  */
+async function getPokemonData<T>(runPokedexNumbers: number[], endPoint: string, signal: AbortSignal): Promise<ResultAsync<T[], FetchError>> {
+  const pokemonDetailResults: Result<T[], FetchError> = await getPokemonDetail(runPokedexNumbers, endPoint, signal);
+  // 一連のfetch中にエラー発生⇒先に戻す
+  if (pokemonDetailResults.isErr()) {
+    const fetchError: FetchError = pokemonDetailResults.error;
+    console.error(`[データ取得失敗] エラータイプ: ${fetchError.type}`, fetchError);
 
-export const movePage = (url: string, setPreURL: setURL, setNextURL: setURL, setIsLoading: setBoolean, setPokemonDetailData: setTypePokemonDetailData): void => {
-  // ロード中にする
-  setIsLoading(true);
+    return pokemonDetailResults; // Errが返る
+  }
 
-  // 指定URLでAPIを実行し内容を表示する
-  asynchroFunction(url, setPreURL, setNextURL, setIsLoading, setPokemonDetailData);
+  return pokemonDetailResults;
+}
+
+//
+//
+// ポケモン個別APIから取得したデータを基にspecies情報検索番号を取得
+/*** @name getSpeciesNumber
+ *   @function arrow
+ *   @param PokemonDetail[]:PokemonDetail[](基本データの配列)
+ *   @return number[]
+ */
+const getSpeciesNumber = (array: PokemonDetail[]): number[] => {
+  const speciesNumbers: number[] = array.map((pokemonDetail) => {
+    return getEndID([pokemonDetail.species])[0];
+    // getEndIDの戻り値は配列
+    // ⇒speciesNumbers(配列)の中に配列を入れるのを防ぐ
+    // ⇒index0の要素を取り出してspeciesNumbersという配列に格納
+  });
+  return speciesNumbers;
+};
+
+//
+//
+// ポケモン個別APIから取得したデータを基に表示・検索・保存に使うデータを整形
+/*** @name createBaseData
+ *   @function arrow
+ *   @param pokemonDetails[]:PokemonDetail[](基本データの配列)
+ *   @param pokemonSpecies[]:PokemonSpeciesDetail[](固有データの配列)
+ *   @param runNumbers[]:number[](対象の管理番号配列)
+ *   @return Promise<ResultAsync<PokemonDetail[], FetchError>>
+ *  ・id:number(管理番号)
+ *  ・name:string(名前)
+ *  ・type:number[](タイプ番号)
+ *  ・pokedex:PokedexNumber(全国図鑑番号)
+ *  ・gene:number(初出世代)
+ *  ・isGen:number(オスメス差分の有無) Number(boolean)で数値化
+ *  ・egg:number[](卵グループ)
+ */
+const createBaseData = (pokemonDetails: PokemonDetail[], pokemonSpecies: PokemonSpeciesDetail[], runNumbers: number[]): LsPokemon[] => {
+  return runNumbers.map((num: number) => {
+    // 管理番号(num)に一致するpokemonDetailsのデータを取得
+    const numPokemonDetail: PokemonDetail | undefined = pokemonDetails.find((detail) => detail.id === num);
+    // 管理番号(num)に一致するpokemonSpeciesのデータを取得
+    const numPokemonSpecies: PokemonSpeciesDetail | undefined = pokemonSpecies.find((species) => {
+      if (numPokemonDetail !== undefined) {
+        // numPokemonDetailを正常に取得
+        // ⇒numPokemonDetailのspeciesと一致するspecies.idを返す
+        return species.id === getEndID([numPokemonDetail.species])[0];
+      } else {
+        return undefined;
+      }
+    });
+
+    // オブジェクトに詰める情報の変数宣言
+    let setName: LsPokemon['name'] = null;
+    let setType: LsPokemon['type'] = null;
+    let setPokedex: LsPokemon['pokedex'] = null;
+    let setSpecies: LsPokemon['sp'] = null;
+    let setRegion: LsPokemon['region'] = null;
+    let setIsGender: LsPokemon['isGen'] = null;
+    let setEgg: LsPokemon['egg'] = null;
+    let setImg: LsPokemon['img'] = null;
+    let setDifNm: LsPokemon['difNm'] = null;
+
+    // PokemonDetailの情報を詰める用に加工
+    if (numPokemonDetail) {
+      // 管理番号と一致するPokemonDetailがある
+
+      // typeの番号を取得
+      setType = numPokemonDetail.types.map((data) => {
+        // 結果が配列でreturn
+        // index:0の結果を受け取る形にする
+        return getEndID([data.type])[0];
+      });
+
+      // 表示用画像を取得
+      setImg = getDisplayImg(numPokemonDetail.sprites);
+
+      // フォルムチェンジなど、特殊姿の場合
+      if (!numPokemonDetail.is_default) {
+        setDifNm = numPokemonDetail.name;
+      }
+    }
+
+    // PokemonSpeciesDetailの情報を詰める用に加工
+    if (numPokemonSpecies) {
+      // 管理番号と一致するPokemonSpeciesDetailがある
+
+      // 日本語名前を取得
+      // PokemonSpeciesDetail['names']が配列定義なので配列のまま処理
+      const tmpName: PokemonSpeciesDetail['names'] = getJaData(numPokemonSpecies.names);
+
+      // オプショナルチェイニング記法も有だが手堅い三項演算子で処理
+      setName = tmpName && tmpName.length > 0 ? tmpName[0].name : null; // tmpNameは配列扱い＝index:0を指定
+      console.log({ setName });
+
+      // 全国図鑑番号を取得
+      const tmpPokedex: PokemonSpeciesDetail['pokedex_numbers'] = getNationalData(numPokemonSpecies.pokedex_numbers);
+      // 数値を取り出して型変換
+      setPokedex = tmpPokedex[0].entry_number as PokedexNumber;
+
+      // Speciesの値を詰める
+      setSpecies = numPokemonSpecies.id;
+
+      // 登場する図鑑（全国図鑑を除く）
+      setRegion = getPokedexNumber(numPokemonSpecies.pokedex_numbers);
+
+      // オスメス差分の有無取得
+      setIsGender = Number(numPokemonSpecies.has_gender_differences);
+
+      // 卵グループ取得
+      setEgg = getEndID(numPokemonSpecies.egg_groups);
+
+      //
+    }
+
+    // 取得したデータから必要情報をオブジェクトに詰める
+    const toLSObject: LsPokemon = {
+      id: num,
+      name: setName,
+      type: setType,
+      pokedex: setPokedex,
+      sp: setSpecies,
+      region: setRegion,
+      isGen: setIsGender,
+      egg: setEgg,
+      img: setImg,
+      difNm: setDifNm,
+    };
+
+    // 作成したオブジェクトを返す
+    return toLSObject;
+  });
+};
+
+//
+//
+// ポケモン個別APIで詳細データを取得する
+/*** @name backgroundFetchAPI
+ *   @function async/await
+ *   @param pokedexNumArray[]:number[](全国図鑑の番号)
+ *   @param getAPIcount:number(１回のAPI実行件数)
+ *   @param pokemonData:LsPokemon[](表示・検索用ポケモンデータ
+ *   @param setPokemonData:setPokemonData(API取得データ,useState)
+ *   @param signal:AbortSignal fetch操作を止めるシグナル
+ *   @return Promise<void> 戻り値なし
+ *  ・詳細データの取得
+ */
+const backgroundFetchAPI = async (pokedexNumArray: number[], getAPIcount: number, pokemonData: LsPokemon[], setPokemonData: setPokemonData, signal: AbortSignal): Promise<void> => {
+  const startNum: number = 0 + getAPIcount; // ローディングの裏で取得した分の続きから開始
+
+  for (let i: number = startNum; i <= pokedexNumArray.length - startNum; i += getAPIcount) {
+    await getNowPokemonData(pokedexNumArray, i, getAPIcount, pokemonData, setPokemonData, signal);
+  }
+  console.log('backgroundFetchAPI finished');
+};
+
+//
+//
+// APIから取得したデータをローカルストレージに追加更新する関数
+/*** @name updateLsData
+ *   @function arrow
+ *   @param regLsData:LsPokemon[](登録するオブジェクト配列)
+ *   @return void
+ *
+ */
+const updateLsData = (regLsData: LsPokemon[]): void => {
+  // ローカルストレージの既存データを取得
+  const currentLsData = localStorage.getItem('pokemonData');
+
+  // 既存データがあればJSON変換
+  // 無い：「成功」の空配列（ok<LsPokemon[], FetchError>([])）を返す
+  const pokemonDataResult: Result<LsPokemon[], FetchError> = currentLsData ? parseJsonBody<LsPokemon[]>(currentLsData, 'localStorage:pokemonData') : ok<LsPokemon[], FetchError>([]);
+
+  // 失敗しててもok([])の結果を返す
+  // 成功してたらResult<LsPokemon[]の結果を取り出して（unwrapOr）渡す
+  const currentLsDataJSON: LsPokemon[] = pokemonDataResult.unwrapOr([]);
+
+  pokemonDataResult.match(
+    (pokemonData: LsPokemon[]) => {
+      console.log('Jsonパース成功', pokemonData);
+
+      // 既存のデータに対し結合・ソート・一意化
+      const mergeAndSortJson = mergeAndUniqueById(currentLsDataJSON, regLsData);
+
+      console.log({ mergeAndSortJson });
+
+      // マージしたオブジェクト配列を文字列json化してローカルストレージのデータに上書き
+      const setPokemonDataJson = JSON.stringify(mergeAndSortJson);
+      localStorage.setItem('pokemonData', setPokemonDataJson);
+    },
+    (resultError: FetchError) => {
+      // localStorage.removeItem('pokemonData');
+      console.log(`Jsonパースに失敗しました。詳細は以下の通りです。
+      \n通信先：${resultError.context?.url},
+      \nエラータイプ：${resultError.type},
+      \n通信ステータス：${resultError.status},
+      \nメッセージ：${resultError.message},
+      \nエラーボディ：${resultError.context?.responseSnippet}`);
+    },
+  );
+};
+
+//
+//
+// APIから取得したデータをメモリ上に直接追加・更新する関数
+// ※ローカルストレージが使用できない場合用の関数
+/*** @name addDataToMemory
+ *   @function arrow
+ *   @param regLsData:LsPokemon[](登録するオブジェクト配列)
+ *   @param setPokemonData:setPokemonData(API取得データ,useState)
+ *   @return void
+ */
+const addDataToMemory = (regLsData: LsPokemon[], pokemonData: LsPokemon[], setPokemonData: setPokemonData): void => {
+  // 現在のAPIから取得したデータと格納済みのデータを結合・id順にソートして変数に格納
+
+  const mergeAndSortJson = mergeAndUniqueById(pokemonData, regLsData);
+  setPokemonData(mergeAndSortJson);
+  console.log(pokemonData);
 };
--- src/utilities/utilityFunction.tsx
+++ src/utilities/utilityFunction.tsx
@@ -1,8 +1,9 @@
 /**
  * 各種パーツとして使用する関数を記述するファイル
  */
-import type { PokedexNumber } from './typesUtility';
-import type { NameAndURL, PokemonSpeciesDetail } from './typesFetch';
+import type { NameAndURL, PokemonSpeciesDetail, PokemonDetail } from './typesFetch';
+import { commonImgURL } from './dataInfo';
+import type { LsPokemon } from './typesUtility';
 
 /**
  * ローカル/セッションストレージが使用可能か確認する関数
@@ -55,18 +56,8 @@ export function getEndID<T extends { url: string }>(results: T[]): number[] {
     return Number(parts[parts.length - 2]);
   });
 
-  // 配列に格納された全図鑑番号を返す
+  // 配列に格納されたIDの配列を返す
   return endIDs;
-}
-
-/**
- * number型の値をPokedexNumber型にキャストする関数
- * @param pokeNum:number 図鑑番号として扱いたい数値
- * @returns PokedexNumber型にキャストされた数値
- */
-export function toPokedexNumber(pokeNum: number): PokedexNumber {
-  // 実行時には単純な数値ですが、型システム上はブランドが付与されます
-  return pokeNum as PokedexNumber;
 }
 
 /**
@@ -90,6 +81,65 @@ export function getJaData<T extends { language: NameAndURL }>(apiArray: T[]): T[
   // どちらも該当しない→空配列を返す
   return [];
 }
+
+/**
+ * 配列の中から全国図鑑のオブジェクトを取得する関数
+ * @param apiArray:PokemonSpeciesDetail[] pokedexを含む型の配列
+ * @returns PokemonSpeciesDetail[]:実質1要素だけ返す（null=[]で処理)
+ */
+export function getNationalData(apiArray: PokemonSpeciesDetail['pokedex_numbers']): PokemonSpeciesDetail['pokedex_numbers'] {
+  // 言語がnational(全国図鑑)に一致するものを返す
+  const national = apiArray.find((item) => item.pokedex.name === 'national');
+  // jaが存在した時点で返す
+  if (national) return [national]; // 配列形式に変換して返す
+
+  // どちらも該当しない→空配列を返す
+  return [];
+}
+
+/**
+ * 配列の中から全国図鑑の以外オブジェクトを取得する関数
+ * @param apiArray:PokemonSpeciesDetail[] pokedexを含む型の配列
+ * @returns PokemonSpeciesDetail[]:実質1要素だけ返す（null=[]で処理)
+ */
+export const getPokedexNumber = (nums: PokemonSpeciesDetail['pokedex_numbers']): number[] => {
+  // 図鑑リストから全国図鑑を抜く
+  const pokedexExNational: PokemonSpeciesDetail['pokedex_numbers'] = nums.filter((num) => num.pokedex.name !== 'national');
+
+  // 全国図鑑以外の番号を取得
+  const pokedex: number[] = pokedexExNational.map((obj) => {
+    // pokedex.urlの末尾から番号取得
+    return Number(getEndID([obj.pokedex]));
+  });
+
+  return pokedex;
+};
+
+/**
+ * オブジェクトから表示可能な画像を取得する関数
+ * @param obj:PokemonDetail['sprites']
+ * @returns string(URLの可変部分)
+ * home > official-artwork > front_default > showdown の順に確認
+ */
+export const getDisplayImg = (obj: PokemonDetail['sprites']): string | null => {
+  // URL共通部分（切り出し部分）
+  const commonURL: string = commonImgURL;
+
+  // 1. homeがあったらその時点で返す
+  if (obj.other.home.front_default !== null) return obj.other.home.front_default?.split(commonURL)[1];
+
+  // 2. official-artworkがあったらその時点で返す
+  if (obj.other['official-artwork'].front_default !== null) return obj.other['official-artwork'].front_default.split(commonURL)[1];
+
+  // 3. 直下のデータがあったらその時点で返す
+  if (obj.front_default !== null) return obj.front_default.split(commonURL)[1];
+
+  // 4. gifのデータがあったら返す（最終手段）
+  if (obj.other.showdown.front_default !== null) return obj.other.showdown.front_default.split(commonURL)[1];
+
+  // 本当に何もないときはnullで返す
+  return null;
+};
 
 /**
  * 指定idのnull埋めオブジェクトを作成
@@ -147,3 +197,41 @@ export const createNullSpecies = (id: number): PokemonSpeciesDetail => ({
     url: '',
   },
 });
+
+/**
+ * 2つの配列を結合＋idの昇順に並べ替え＋idの重複を削除
+ * @param currentArray:LsPokemon[] 元の配列
+ * @param addArray:LsPokemon[] 追加する配列
+ * @returns LsPokemon[]:整理が終わった配列
+ */
+export const mergeAndUniqueById = (currentArray: LsPokemon[], addArray: LsPokemon[]): LsPokemon[] => {
+  // 既存データに取得したデータをマージ
+  // currentLsDataJSONの配列、regLsDataの配列を解体＋全て結合
+  // ※「...」のスプレッド構文を使用
+  // ⇒元のcurrentLsDataJSON配列を破壊せず、新しい配列としてupdateDataが作成される
+  const mergeData: LsPokemon[] = [...currentArray, ...addArray];
+
+  // スプレッド構文で配列を複製＋id順に並べ直す
+  // idの中身a,bを比較して、小さい方を前に配列の前方要素に移していく
+  const sortData: LsPokemon[] = [...mergeData].sort((a, b) => a.id - b.id);
+
+  // idが重複している要素があったら削除
+  // ⇒まだ見たことがないIDならMapに記録する
+  const uniqueData: LsPokemon[] = Array.from(
+    sortData
+      .reduce((map, pokemonData) => {
+        // map=処理の集積
+        // pokemonData=各要素
+        if (!map.has(pokemonData.id)) {
+          // 集積結果の中に今処理してるid値が存在して「ない」
+          map.set(pokemonData.id, pokemonData);
+          // 未登録ならこの場で登録＝一意にする
+        }
+        return map; // 一意に整理した集積結果を返す
+      }, new Map<number, LsPokemon>())
+      // id:number, pokemonData:LsPokemonの明示
+      .values(), // reduce().valueの形式
+  );
+
+  return uniqueData;
+};
