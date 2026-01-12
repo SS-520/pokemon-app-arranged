a52f5079c76b54ffbb170e6af9637a61d8467744
rin <sayuri.shindo@gmail.com>
2026-01-12 20:42:01 +0900

    呼び出しファイル修正・setNumber追加

--- src/utilities/types/typesUtility.tsx
+++ src/utilities/types/typesUtility.tsx
@@ -1,13 +1,17 @@
 // ユーザー定義の型の集積ファイル
 
 import type { Dispatch, SetStateAction } from 'react';
-import type { PokemonListResponse, PokemonDetail } from '../types-fetch';
+import type { PokemonListResponse, PokemonDetail } from './typesFetch';
 // ＊外部から呼び出すのが全体の定義⇒export を付与
 
 //// 状態変数の定義
 // useStateのの中身を更新するための型
 export type setURL = Dispatch<SetStateAction<PokemonListResponse['previous']> | PokemonListResponse['next']>;
+
 export type setBoolean = Dispatch<SetStateAction<boolean>>;
+
+export type setNumber = Dispatch<SetStateAction<number>>;
+
 export type setTypePokemonDetailData = Dispatch<SetStateAction<PokemonDetail[]>>;
 
 export interface BallDetails {
